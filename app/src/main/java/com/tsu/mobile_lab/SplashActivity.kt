7b704a62d5c5350290f10f8de6e4f59836fcc632
SRoman <slivkin.roma@yandex.ru>
2023-03-04 23:12:05 +0700

    1.2

--- app/src/main/java/com/tsu/mobile_lab/SplashActivity.kt
+++ app/src/main/java/com/tsu/mobile_lab/SplashActivity.kt
@@ -1,11 +1,20 @@
 package com.tsu.mobile_lab
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java) )
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, 2000)
     }
 }
