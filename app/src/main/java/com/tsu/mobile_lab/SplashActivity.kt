484f3665fd547694b6c305408d7c21be2736c895
SRoman <slivkin.roma@yandex.ru>
2023-03-04 22:36:22 +0700

    1.2

---
+++ app/src/main/java/com/tsu/mobile_lab/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.tsu.mobile_lab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
