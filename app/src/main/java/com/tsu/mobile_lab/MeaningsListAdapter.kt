ca09900ef121bae990cf2aa601141dcedfb0421d
SRoman <slivkin.roma@yandex.ru>
2023-03-22 17:43:31 +0700

    2.2

---
+++ app/src/main/java/com/tsu/mobile_lab/MeaningsListAdapter.kt
@@ -0,0 +1,37 @@
+package com.tsu.mobile_lab
+
+import android.text.SpannableString
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.tsu.mobile_lab.databinding.ItemMeaningsListBinding
+
+class MeaningsListAdapter(private val meanings: MutableList<SpannableString>): RecyclerView.Adapter<MeaningsListAdapter.MeaningsViewHolder>() {
+
+    class MeaningsViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        private val viewBinding = ItemMeaningsListBinding.bind(view)
+
+        fun bind(text: SpannableString) {
+            viewBinding.itemTextView.text = text
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MeaningsViewHolder {
+        var view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_meanings_list, parent, false)
+
+        return MeaningsViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return meanings.size
+    }
+
+    override fun onBindViewHolder(holder: MeaningsViewHolder, position: Int) {
+        holder.bind(meanings[position])
+    }
+
+
+}
--- app/src/main/java/com/tsu/mobile_lab/ui/dictionary/DictionaryFragment.kt
+++ app/src/main/java/com/tsu/mobile_lab/ui/dictionary/DictionaryFragment.kt
@@ -12,11 +12,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.text.toSpannable
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.savedstate.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tsu.mobile_lab.DictionaryAPI
+import com.tsu.mobile_lab.MainAppActivity
+import com.tsu.mobile_lab.MeaningsListAdapter
 import com.tsu.mobile_lab.databinding.FragmentDictionaryBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -44,6 +48,7 @@ class DictionaryFragment : Fragment() {
         _binding = FragmentDictionaryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        var list = mutableListOf<SpannableString>()
         val newLine: String = "\n"
         val exampleText: String = "Example: "
         var wordInfo: String = "The practice or skill of preparing food by combining, mixing, and heating ingredients."
@@ -59,8 +64,8 @@ class DictionaryFragment : Fragment() {
         coloredExample.setSpan(fcsBlue, lenWordInfo, lenWordInfo + 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
 
-        binding.textView10.text = coloredExample
-        binding.textView11.text = coloredExample
+        //binding.textView10.text = coloredExample
+        //binding.textView11.text = coloredExample
         /*val textView: TextView = binding.textView10
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
@@ -80,13 +85,14 @@ class DictionaryFragment : Fragment() {
 
             val service = retrofit.create(DictionaryAPI::class.java)
 
-
+            list.clear()
 
             lifecycleScope.launch(Dispatchers.IO) {
 
                 var result = service.getWordInfo(userWord)//.toString()
                 var word = result[0]
                 var phon = word.phonetics
+
 
                 withContext(Dispatchers.Main){
                     //binding.textView10.text = result[0].meanings[0].definitions[0].definition
@@ -103,8 +109,41 @@ class DictionaryFragment : Fragment() {
 
                     binding.transcriptionTextView.text = "[" + transcription + "]"
 
+                    result[0].meanings[0].definitions.forEach(){
 
-                    var example: String = result[0].meanings[0].definitions[0].example
+                        var example: String = it.example
+                        if (example != null) {
+                            var wordInfo: String = it.definition
+                            val lenWordInfo: Int = wordInfo.count() + 2
+                            //val info = wordInfo.substring("skill")
+                            //var exampleInfo: String = result[0].meanings[0].definitions[0].example
+                            val totalWordInfo =
+                                wordInfo + newLine + newLine + exampleText + example
+                            var coloredExample = SpannableString(totalWordInfo)
+                            var fcsBlue = ForegroundColorSpan(
+                                Color.parseColor("#65AAEA")
+                            )
+
+                            coloredExample.setSpan(
+                                fcsBlue,
+                                lenWordInfo,
+                                lenWordInfo + 9,
+                                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+
+                            list.add(coloredExample)
+                        }
+                        else {
+                            val spanDef = SpannableString(it.definition)
+                            //binding.textView10.text = it.definition
+                            list.add(spanDef)
+                        }
+                    }
+                    Log.d("Fragment", list[0].toString())
+                    binding.meanRecyclerView.layoutManager = LinearLayoutManager(MainAppActivity())
+                    binding.meanRecyclerView.adapter = MeaningsListAdapter(list)
+
+                   /* var example: String = result[0].meanings[0].definitions[0].example
                     if (example != null){
                         var wordInfo: String = result[0].meanings[0].definitions[0].definition
                         val lenWordInfo: Int = wordInfo.count() + 2
@@ -120,13 +159,18 @@ class DictionaryFragment : Fragment() {
                         binding.textView10.text = coloredExample
                     }
 
-                    else binding.textView10.text = result[0].meanings[0].definitions[0].definition
+                    else binding.textView10.text = result[0].meanings[0].definitions[0].definition*/
 
                 }
+
+
 
                 Log.d("Fragment", result.toString())
             }
         }
+
+
+
 
 
 
